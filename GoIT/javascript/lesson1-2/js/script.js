117b1273053a23072de357ef856cfb705fe8da42
Speariv35 <speariv35@gmail.com>
2015-12-01 18:36:57 +0200

    Add some fixes for JS HW 1-2

--- GoIT/javascript/lesson1-2/js/script.js
+++ GoIT/javascript/lesson1-2/js/script.js
@@ -1,28 +1,22 @@
 function pow(x, y) {
-  var result = x;
+  var result = 1;
 
-  if (y > 0) {
-    for (var i = 1; i < y; i++) {
+  if (y >= 0) {
+    for (var i = 0; i < y; i++) {
     result *= x;
   }
     return result;
-  } else if (y < 0) {
-    for (var i = -1; i > y; i--) {
+  } else  {
+    for (var i = 0; i > y; i--) {
     result *= x;
-}
+    }
     result = 1 / result;
     return result;
-  }
-  else {
-    result = 1;
-    return result;
-  }
-
+    }
 }
 
 var x = prompt('Input x ', x);
 var y = prompt('Input y ', y);
-
 
 alert( pow(x, y) );
 console.log ('x pow y = ', pow(x, y))
