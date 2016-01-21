cb964f11d2d12ae6b3bd731ee5923f4a3e146c59
Speariv35 <speariv35@gmail.com>
2016-01-21 12:01:31 +0200

    Add part of HW Javascript 11-12

---
+++ GoIT/javascript/lesson11-12/js/script.js
@@ -0,0 +1,37 @@
+
+$(function() {
+    $prev = $('.prev');
+    $next = $('.next');
+    var elemList = $('.carousel');
+
+   var pixelsShift = 200;
+   var leftPosition = 0;
+   var carouselWidth = $('.hide').css('width');
+ 	var elementsCount = elemList.find('li').length;
+
+	var minShift = -2000+600/*- (elementsCount - (carouselWidth / pixelsShift)) * pixelsShift);*/
+   var maxShift = 0;
+
+    $prev.click(function() {
+    	 if (leftPosition != maxShift) {
+    	 	$prev.removeClass("inactive");
+    	 	$next.removeClass("inactive");
+    		leftPosition += pixelsShift;
+        elemList.animate({ left : leftPosition + "px"}, 500);
+     }  else {
+        $prev.addClass("inactive");
+        }
+    });
+ 
+    $next.click(function() {
+    	  if (leftPosition != minShift) {
+    	  		$next.removeClass("inactive");
+    	  		$prev.removeClass("inactive");
+    			leftPosition -= pixelsShift;
+        		elemList.animate({ left : leftPosition + "px"}, 500);
+        } else {
+        $next.addClass("inactive");
+        }
+    });
+ 
+});
